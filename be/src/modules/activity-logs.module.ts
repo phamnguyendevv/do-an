e3d7b2477afd24ceb8f68d7c79672b32713e4967
Phàn Pà Vảng <laokaifptshop@gmail.com>
2026-09-03 23:20:41 +0700

    Update be/src/modules/activity-logs.module.ts

---
+++ be/src/modules/activity-logs.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ActivityLogsController } from '@adapters/controllers/activity-logs/activity-logs.controller'
+import { ActivityLog } from '@infrastructure/databases/postgresql/entities/activity-log.entity'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ActivityLog])],
+  controllers: [ActivityLogsController],
+})
+export class ActivityLogsModule {}
